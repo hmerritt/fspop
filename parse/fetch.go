92f19251f2859a061113b0a3ba0ce7513f91370f
Harry Merritt <harry@merritt.es>
2021-04-24 02:16:40 +0100

    Unify fetch functions into one
    Use one function to fetch data
    - Assumes local file
    - Auto detect URLs

--- parse/fetch.go
+++ parse/fetch.go
@@ -3,7 +3,31 @@ package parse
 import (
 	"net/http"
 	"os"
+	"strings"
 )
+
+func IsUrl(str string) bool {
+	if strings.HasPrefix(str, "www.") || strings.HasPrefix(str, "http://") || strings.HasPrefix(str, "https://") {
+		return true
+	}
+	return false
+}
+
+func UseUrl(str string) bool {
+	if _, err := os.Stat("/path/to/whatever"); os.IsNotExist(err) {
+		return IsUrl(str)
+	}
+
+	return false
+}
+
+func FetchYaml(path string) []byte {
+	if UseUrl(path) {
+		return FetchUrl(path)
+	} else {
+		return FetchFile(path)
+	}
+}
 
 func FetchFile(filepath string) []byte {
 	// Read file

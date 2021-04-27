19a95e09f2e82aa84d5c2965dd354c2eec3589fc
Harry Merritt <harry@merritt.es>
2021-04-27 22:54:17 +0100

    Find and return an Item via it's Path

--- structure/structure.go
+++ structure/structure.go
@@ -1,6 +1,7 @@
 package structure
 
 import (
+	"errors"
 	"strings"
 )
 
@@ -52,4 +53,39 @@ func StandardizeDirectory(path string) string {
 		path = path[1:] + "/"
 	}
 	return path
+}
+
+/*
+ * Find an FspopItem through it's Path
+ * would like to use a recursive function for this but would require
+ * an extra param such as, Find(items *[]FspopItem, ...) which is
+ * not great to use.
+ *
+ */
+func (fsStruct *FspopStructure) Find(pathToFind *FspopStructurePath) (*FspopItem, error) {
+	items := fsStruct.Items
+	loop := true
+
+	// Traverse until a find, or til end
+	for loop {
+		// Loop FspopItem slice
+		for _, i := range items {
+			// Match path
+			if i.Path.ToString() == pathToFind.ToString() {
+				// Found!
+				i.Data = "changed"
+				return i, nil
+
+			} else if len(i.Children) > 0 {
+				// Recurse deeper if item has children
+				items = i.Children
+			} else {
+				// Item not found, end loop
+				loop = false
+			}
+		}
+	}
+
+	// Path not found :(
+	return &FspopItem{}, errors.New("path not found")
 }

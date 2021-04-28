07f20b3568515ee89456883c8ad9e524157d1b60
Harry Merritt <harry@merritt.es>
2021-04-28 04:38:47 +0100

    Remove out-of-date mock structures in display command

--- command/display.go
+++ command/display.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strings"
 
+	"gitlab.com/merrittcorp/fspop/message"
+	"gitlab.com/merrittcorp/fspop/parse"
 	"gitlab.com/merrittcorp/fspop/structure"
 )
 
@@ -29,122 +31,57 @@ Usage: fspop display [options] NAME
 }
 
 func (c *DisplayCommand) Run(args []string) int {
-	// var path string
+	var path string
 
-	// if len(args) == 0 {
-	// 	message.Warn("No file entered.")
-	// 	message.Warn("Trying default '" + parse.DefaultYamlFile + "' instead.")
-	// 	message.Text("")
-	// 	path = parse.DefaultYamlFile
-	// } else {
-	// 	path = parse.ElasticExtension(args[0])
-	// }
-
-	// var fileData []byte
-	// var fileError error
-
-	// // Decide if URL or file
-	// if parse.UseUrl(path) {
-	// 	message.Spinner.Start("", " Fetching URL data...")
-
-	// 	fileData, fileError = parse.FetchUrl(path)
-
-	// 	message.Spinner.Stop()
-
-	// 	if fileError != nil {
-	// 		message.Error("Unable to fetch URL data.")
-	// 		message.Error(fmt.Sprint(fileError))
-	// 		fmt.Println()
-	// 		message.Warn("Make sure the link is accessible and try again.")
-	// 		return 2
-	// 	}
-	// } else {
-	// 	fileData, fileError = parse.FetchFile(path)
-
-	// 	if fileError != nil {
-	// 		message.Error("Unable to open file.")
-	// 		message.Error(fmt.Sprint(fileError))
-	// 		fmt.Println()
-	// 		message.Warn("Check the file is exists and try again.")
-	// 		return 2
-	// 	}
-	// }
-
-	// fsStructure := parse.ParseAndRefineYaml(fileData)
-	// //parse.ParseAndRefineYaml(fileData)
-
-	// // fmt.Println(fsStructure)
-
-	// Mock children items
-	fsMockChildrenItems := make([]*structure.FspopItem, 0)
-	fsMockChildrenItems = append(fsMockChildrenItems, &structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"disone/", "file.wow"}),
-		IsDir:      false,
-		IsEndpoint: true,
-		HasData:    false,
-	})
-	fsMockChildrenItems = append(fsMockChildrenItems, &structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"disone/", "2dir/"}),
-		IsDir:      true,
-		IsEndpoint: false,
-		HasData:    false,
-		Data:       "oh no",
-	})
-
-	// Mock items
-	fsMockItems := make([]*structure.FspopItem, 0)
-	fsMockItems = append(fsMockItems, &structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"file.wow"}),
-		IsDir:      false,
-		IsEndpoint: true,
-		HasData:    false,
-	})
-	fsMockItems = append(fsMockItems, &structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"anotherfile.waw"}),
-		IsDir:      false,
-		IsEndpoint: true,
-		HasData:    false,
-	})
-	fsMockItems = append(fsMockItems, &structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"firstdir/"}),
-		IsDir:      true,
-		IsEndpoint: false,
-		HasData:    false,
-	})
-	fsMockItems = append(fsMockItems, &structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"disone/"}),
-		IsDir:      true,
-		IsEndpoint: false,
-		HasData:    false,
-		Children:   fsMockChildrenItems,
-	})
-
-	// Mock structure
-	fsMockStructure := &structure.FspopStructure{
-		Version: "2",
-		Name:    "Mock structure",
-		Items:   fsMockItems,
+	if len(args) == 0 {
+		message.Warn("No file entered.")
+		message.Warn("Trying default '" + parse.DefaultYamlFile + "' instead.")
+		message.Text("")
+		path = parse.DefaultYamlFile
+	} else {
+		path = parse.ElasticExtension(args[0])
 	}
 
-	// = & fsMockStructure[item]
-	// getItem, _ := fsMockStructure.Find(structure.CreateFspopPath([]string{"file.wow"}))
-	// getItem.Data = "wooo"
-	// fmt.Println(*getItem)
-	// fmt.Println(fsMockStructure.Items[0])
+	var fileData []byte
+	var fileError error
 
-	_, err := fsMockStructure.Find(structure.CreateFspopPath([]string{"disone/", "2dir/", "wownew/"}))
-	fmt.Println(err)
+	// Decide if URL or file
+	if parse.UseUrl(path) {
+		message.Spinner.Start("", " Fetching URL data...")
 
-	fsMockStructure.Add(&structure.FspopItem{
-		Path:       *structure.CreateFspopPath([]string{"disone/", "2dir/", "wownew/"}),
-		IsDir:      true,
-		IsEndpoint: true,
-		HasData:    false,
+		fileData, fileError = parse.FetchUrl(path)
+
+		message.Spinner.Stop()
+
+		if fileError != nil {
+			message.Error("Unable to fetch URL data.")
+			message.Error(fmt.Sprint(fileError))
+			fmt.Println()
+			message.Warn("Make sure the link is accessible and try again.")
+			return 2
+		}
+	} else {
+		fileData, fileError = parse.FetchFile(path)
+
+		if fileError != nil {
+			message.Error("Unable to open file.")
+			message.Error(fmt.Sprint(fileError))
+			fmt.Println()
+			message.Warn("Check the file is exists and try again.")
+			return 2
+		}
+	}
+
+	fsStructure := parse.ParseAndRefineYaml(fileData)
+	//parse.ParseAndRefineYaml(fileData)
+
+	fmt.Println(fsStructure.Items)
+
+	fmt.Print("\n\n\n")
+
+	fsStructure.Crawl(func(key string, item structure.FspopItem) {
+		fmt.Printf("%v  %s  :  %s\n", (key == item.Path.ToString()), key, item.Path.ToString())
 	})
-
-	path, err2 := fsMockStructure.Find(structure.CreateFspopPath([]string{"disone/", "2dir/", "wownew/"}))
-	fmt.Println(err2)
-	fmt.Println(path)
 
 	return 0
 }

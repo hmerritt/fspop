97ba70303acfbbe539353e8d67cf186310122747
Harry Merritt <harry@merritt.es>
2021-05-27 22:07:37 +0100

    Create single fetch function for fetching yaml from file or url

--- parse/fetch.go
+++ parse/fetch.go
@@ -2,10 +2,12 @@ package parse
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/imroc/req"
+	"gitlab.com/merrittcorp/fspop/message"
 )
 
 func IsUrl(path string) bool {
@@ -19,14 +21,6 @@ func UseUrl(path string) bool {
 func FileExists(path string) bool {
 	stat, err := os.Stat(path)
 	return err == nil && !stat.IsDir()
-}
-
-func FetchYaml(path string) ([]byte, error) {
-	if UseUrl(path) {
-		return FetchUrl(path)
-	} else {
-		return FetchFile(path)
-	}
 }
 
 func FetchFile(filepath string) ([]byte, error) {
@@ -49,10 +43,43 @@ func FetchUrl(url string) ([]byte, error) {
 	return res.Bytes(), nil
 }
 
+// Fetch and parse YAML file locally or from a URL
+func FetchYaml(path string) []byte {
+	var data []byte
+	var err error
+
+	// Decide if URL or file
+	if UseUrl(path) {
+		message.Spinner.Start("", " Fetching URL data...")
+
+		data, err = FetchUrl(path)
+
+		message.Spinner.Stop()
+
+		if err != nil {
+			message.Error("Unable to fetch URL data.")
+			message.Error(fmt.Sprint(err))
+			fmt.Println()
+			message.Warn("Make sure the link is accessible and try again.")
+			os.Exit(2)
+		}
+	} else {
+		data, err = FetchFile(path)
+
+		if err != nil {
+			message.Error("Unable to open file.")
+			message.Error(fmt.Sprint(err))
+			fmt.Println()
+			message.Warn("Check the file is exists and try again.")
+			os.Exit(2)
+		}
+	}
+
+	return data
+}
+
 // Create file and return open file object
 func CreateFile(path string) (*os.File, error) {
 	file, err := os.Create(path)
 	return file, err
 }
-
-//func HttpStatusCodeError

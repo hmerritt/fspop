47ebf28be8c95b28a250823367a291b616bf0df3
Harry Merritt <harry@merritt.es>
2021-05-27 18:21:16 +0100

    Traverse structure item endpoints as FspopItem in unrefined yaml

--- structure/structure_traverse.go
+++ structure/structure_traverse.go
@@ -70,3 +70,78 @@ func (fsYamlStruct *YamlStructure) TraverseDynamic(callback func(FspopDynamic)) 
 		}
 	}
 }
+
+// Traverse each structure endpoint as an 'FspopItem'
+// note: only endpoints are traversed and not each stage of the structure
+func (fsYamlStruct *YamlStructure) TraverseStructure(callback func(FspopItem)) {
+	// Crawl each endpoint recursivly
+	crawlYamlStructureItems(fsYamlStruct.Structure, *CreateFspopPath([]string{}), callback)
+}
+
+// Crawl through the 'structure:' items key in the messy parsed yaml
+// figuring out whats-what and organising it one item at a time.
+// Will detect and output file data and dynamic keys.
+func crawlYamlStructureItems(structureInterface interface{}, pathStart FspopPath, callback func(FspopItem)) {
+	// Unique path for each iteration
+	// Make a deep copy of path array
+	path := FspopPath{
+		Path: deepCopy(pathStart.Path),
+	}
+
+	switch structureInterface.(type) {
+	case string:
+		itemName := fmt.Sprintf("%v", structureInterface)
+		dynamicKey := ""
+
+		// File or Directory name
+		path.Append(itemName)
+
+		// Check for a dynamic key
+		if !IsDirectory(itemName) && strings.HasPrefix(itemName, "$") {
+			dynamicKey = itemName
+		}
+
+		callback(FspopItem{
+			Path:       path,
+			DataKey:    "",
+			DynamicKey: dynamicKey,
+		})
+
+	case []interface{}:
+		// Use type assertion to loop over []interface{}
+		for _, v := range structureInterface.([]interface{}) {
+			crawlYamlStructureItems(v, path, callback)
+		}
+
+	case map[interface{}]interface{}:
+		// Use type assertion to loop over map[string]interface{}
+		for key, value := range structureInterface.(map[interface{}]interface{}) {
+			// Interface 'key' is a directory name
+			// Create a new unique path for each iteration,
+			// prevents 'path' being carried forward and messing
+			// with the callback later.
+			path.Append(fmt.Sprintf("%v", key))
+
+			// Check for file with a data variable
+			// Use 'key' string value as data key
+			if !IsDirectory(fmt.Sprintf("%v", key)) {
+				dataKey := fmt.Sprintf("%v", value)
+				callback(FspopItem{
+					Path:       path,
+					DataKey:    dataKey,
+					DynamicKey: "",
+				})
+				continue
+			}
+
+			crawlYamlStructureItems(value, path, callback)
+		}
+	}
+}
+
+// Make a deep copy of a string slice
+func deepCopy(arr []string) []string {
+	newArr := make([]string, len(arr))
+	copy(newArr, arr)
+	return newArr
+}

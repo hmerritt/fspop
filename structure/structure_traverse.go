f8337956f04ea593fa0e4524fbc62b83482b1979
Harry Merritt <harry@merritt.es>
2021-05-27 18:20:24 +0100

    Traverse dynamic item maps in unrefined yaml

--- structure/structure_traverse.go
+++ structure/structure_traverse.go
@@ -2,6 +2,7 @@ package structure
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Traverse each data key & value in an unrefined YAML structure
@@ -19,6 +20,53 @@ func (fsYamlStruct *YamlStructure) TraverseData(callback func(FspopData)) {
 				Key:  fmt.Sprint(key),
 				Data: fmt.Sprint(value),
 			})
+		}
+	}
+}
+
+// Traverse each dynamic key map in an unrefined YAML structure
+func (fsYamlStruct *YamlStructure) TraverseDynamic(callback func(FspopDynamic)) {
+	// Skip traversal if data key is empty
+	if fsYamlStruct.Dynamic == nil {
+		return
+	}
+
+	// Iterate each dynamic variable map individually
+	for _, dynamicMap := range fsYamlStruct.Dynamic.([]interface{}) {
+		// Get dynamic key and it's values in map form
+		for key, dynamicItemMap := range dynamicMap.(map[interface{}]interface{}) {
+			// Create dynamic key struct
+			fsDynamic := FspopDynamic{
+				// Make sure key has '$' prefix
+				// Removes exsting '$' and then adds it back
+				Key: fmt.Sprintf("$%s", strings.TrimPrefix(fmt.Sprint(key), "$")),
+			}
+
+			// Iterate all dynamic values
+			for _, dynamicValueMap := range dynamicItemMap.([]interface{}) {
+				// Get dynamic item variables from map
+				for variable, value := range dynamicValueMap.(map[interface{}]interface{}) {
+					// Ditermine variable name and place value in the correct place
+					switch strings.ToLower(fmt.Sprint(variable)) {
+					case "amount":
+						fsDynamic.Count = value.(int)
+					case "count":
+						fsDynamic.Count = value.(int)
+					case "data":
+						fsDynamic.DataKey = fmt.Sprint(value)
+					case "type":
+						fsDynamic.Type = strings.ToLower(fmt.Sprint(value))
+					case "name":
+						fsDynamic.Name = fmt.Sprint(value)
+					case "padded":
+						fsDynamic.Padded = value.(bool)
+					case "start":
+						fsDynamic.Start = value.(int)
+					}
+				}
+			}
+
+			callback(fsDynamic)
 		}
 	}
 }

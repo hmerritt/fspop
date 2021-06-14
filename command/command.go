2fe7a43204032badc9c6cea9819e2999665d1a74
Harry Merritt <harry@merritt.es>
2021-06-14 15:43:32 +0100

    Create BaseCommand struct which is passed into all commands
    Used to standardize UI output

--- command/command.go
+++ command/command.go
@@ -1,7 +1,8 @@
 package command
 
 import (
-	"log"
+	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/mitchellh/cli"
@@ -13,27 +14,56 @@ func Run() {
 	app := cli.NewCLI("fspop", version.GetVersion().VersionNumber())
 	app.Args = os.Args[1:]
 
+	getBaseCommand := func() *BaseCommand {
+		return &BaseCommand{
+			UI: &cli.ColoredUi{
+				ErrorColor: cli.UiColorRed,
+				WarnColor:  cli.UiColorYellow,
+				Ui: &cli.BasicUi{
+					Reader:      bufio.NewReader(os.Stdin),
+					Writer:      os.Stdout,
+					ErrorWriter: os.Stderr,
+				},
+			},
+		}
+	}
+
 	// Feed active commands to CLI app
 	app.Commands = map[string]cli.CommandFactory{
 		"deploy": func() (cli.Command, error) {
-			return &DeployCommand{}, nil
+			return &DeployCommand{
+				BaseCommand: getBaseCommand(),
+			}, nil
 		},
 		"display": func() (cli.Command, error) {
-			return &DisplayCommand{}, nil
+			return &DisplayCommand{
+				BaseCommand: getBaseCommand(),
+			}, nil
 		},
 		"init": func() (cli.Command, error) {
-			return &InitCommand{}, nil
+			return &InitCommand{
+				BaseCommand: getBaseCommand(),
+			}, nil
 		},
 		"list": func() (cli.Command, error) {
-			return &ListCommand{}, nil
+			return &ListCommand{
+				BaseCommand: getBaseCommand(),
+			}, nil
 		},
 	}
 
 	// Run app
 	exitStatus, err := app.Run()
 	if err != nil {
-		log.Println(err)
+		os.Stderr.WriteString(fmt.Sprint(err))
 	}
 
 	os.Exit(exitStatus)
+}
+
+// Master command type which in present in all commands
+//
+// Used to standardize UI output
+type BaseCommand struct {
+	UI cli.Ui
 }

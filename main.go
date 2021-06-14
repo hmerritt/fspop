023e70f1637a25bb47acf742db0e65431db9ea1d
Harry Merritt <harry@merritt.es>
2021-06-14 16:41:46 +0100

    Move printTitle function out of message package

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"fmt"
+
 	"gitlab.com/merrittcorp/fspop/command"
-	"gitlab.com/merrittcorp/fspop/message"
+	"gitlab.com/merrittcorp/fspop/version"
 )
 
 func main() {
-	message.PrintTitle()
+	printTitle()
 
 	command.Run()
+}
+
+// Prints fspop title + version info
+func printTitle() {
+	// Get version info
+	versionStruct := version.GetVersion()
+
+	// Check if dev release
+	// Hide git-sha for main releases
+	isDev := false
+	if versionStruct.VersionPrerelease != "" {
+		isDev = true
+	}
+
+	// Get full version string
+	versionString := versionStruct.FullVersionNumber(isDev)
+
+	fmt.Println(versionString)
+	fmt.Println("(c) MerrittCorp. All rights reserved.")
+	fmt.Println()
 }

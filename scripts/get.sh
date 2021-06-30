5199c02f6c6107f3def2a019972ad8927c910394
Harry Merritt <harry@merritt.es>
2021-06-30 23:58:03 +0100

    Catch errors in get.sh install script
    Uses '$?' to check if previous command failed and prints error messages accordingly

--- scripts/get.sh
+++ scripts/get.sh
@@ -17,18 +17,43 @@ release_url="--URL TO RELEASES DIRECTORY--"
 arch=$(dpkg --print-architecture)
 
 
+# Print message when a process fails
+onfail () {
+	if [ "${?}" != "0" ]; then
+		echo
+		echo "${1}"
+		exit 1
+	fi
+}
+
+
+# Fetch install dependencies
+sudo apt -qq install wget unzip
+
+
 # Download latest fspop
-sudo wget "${release_url}/${version}/fspop-${version}-linux_${arch}.zip" -O /var/tmp/fspop.zip
+echo
+echo "Downloading fspop from ${release_url}"
+sudo wget --quiet "${release_url}/${version}/fspop-${version}-linux_${arch}.zip" -O /var/tmp/fspop.zip
+echo
+onfail "ERROR: Unable to fetch fspop"
 
 
 # Extract fspop release zip
-sudo unzip /var/tmp/fspop.zip
+sudo unzip -o /var/tmp/fspop.zip -d /var/tmp
+onfail "ERROR: Unable to extract fspop. Make sure to have 'unzip' installed."
+
 sudo rm /var/tmp/fspop.zip
 
 
 # Move to user bin
-sudo mv /usr/bin/fspop
+sudo mv /var/tmp/fspop /usr/bin/fspop
+onfail "ERROR: Unable to move fspop into /usr/bin directory"
 
 
 # Update permissions, make executable
 sudo chmod +x /usr/bin/fspop
+onfail "ERROR: Unable to make fspop executable. chmod +x /usr/bin/fspop"
+
+echo
+echo "Success. fspop is good to go!"

3b29ceebadcfd3b0aef53f38c6b4b2696dcb792b
Harry Merritt <harry@merritt.es>
2021-07-17 17:15:14 +0100

    Improve update script
    * Detect OS platform
    * Improve arch detection using 'uname' command

--- scripts/get.sh
+++ scripts/get.sh
@@ -12,9 +12,67 @@ version="v1.1.0"
 release_url="--URL TO RELEASES DIRECTORY--"
 
 
+# Detect the platform
+OS="$(uname)"
+case $OS in
+  Linux)
+    OS='linux'
+    ;;
+  FreeBSD)
+    OS='freebsd'
+    echo 'OS not supported'
+    exit 2
+    ;;
+  NetBSD)
+    OS='netbsd'
+	echo 'OS not supported'
+    exit 2
+    ;;
+  OpenBSD)
+    OS='openbsd'
+	echo 'OS not supported'
+    exit 2
+    ;;  
+  Darwin)
+    OS='darwin'
+    ;;
+  SunOS)
+    OS='solaris'
+    echo 'OS not supported'
+    exit 2
+    ;;
+  *)
+    echo 'OS not supported'
+    exit 2
+    ;;
+esac
+
+
 # Get device architecture
 # -> amd64 : arm64
-arch=$(dpkg --print-architecture)
+arch="$(uname -m)"
+case "$arch" in
+  x86_64|amd64)
+    arch='amd64'
+    ;;
+  i?86|x86)
+    arch='386'
+    echo 'OS type not supported'
+    exit 2
+    ;;
+  aarch64|arm64)
+    arch='arm64'
+    ;;
+  arm*)
+    arch='arm'
+    echo 'OS type not supported'
+    exit 2
+    ;;
+  *)
+    echo 'OS type not supported'
+    exit 2
+    ;;
+esac
 
 
 # Print message when a process fails
@@ -34,7 +92,7 @@ sudo apt -qq install wget unzip
 # Download latest fspop
 echo
 echo "Downloading fspop from ${release_url}"
-sudo wget --quiet "${release_url}/${version}/fspop-${version}-linux_${arch}.zip" -O /var/tmp/fspop.zip
+sudo wget --quiet "${release_url}/${version}/fspop-${version}-${OS}_${arch}.zip" -O /var/tmp/fspop.zip
 echo
 onfail "ERROR: Unable to fetch fspop"
 
